c3f2893ec9e2e4ae22cc3bee1446cd647754a42e
femiode01 <41710282+femiode01@users.noreply.github.com>
2025-12-01 14:51:11 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,16 +1,3 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--kernel", type=str, default="rbf", help="The kernel type")
-parser.add_argument("--C", type=float, default=1.0, help="The regularization parameter")
-
-args = parser.parse_args()
-
-kernel = args.kernel
-C = args.C
-
-# Train the model
-train_model(kernel, C)
-
-# Evaluate the model
-evaluate_model()
+print(Hello Femi)
